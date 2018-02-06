de07e26c89be1d7aa2cfc743095a2f3b0e26e229
Troy Mass <masstroy21@gmail.com>
2018-02-06 09:38:34 -0600

    Fixed start docker command

--- start-docker.sh
+++ start-docker.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 
 docker rm -f midwest86cup || true
-docker run --name midwest86cup -p 80:80 -v `pwd`/docs:/usr/share/nginx/html:ro -d nginx:alpine
+docker run --name midwest86cup -p 80:80 -v `pwd`:/usr/share/nginx/html:ro -d nginx:alpine
 
 echo "Done: Server listening on http://localhost:80/"
 
