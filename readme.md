0f4fa667eeb83bbd8db9d3565277809629fe6a18
AJ Schalund <aschalund@thepei.com>
2018-02-09 00:11:13 -0600

    Cleansing branch.

--- readme.md
+++
@@ -1,1 +0,0 @@
-This repo contains code and content for the Midwest 86 Cup website
--- start-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`
-
-docker rm -f midwest86cup || true
-docker run --name midwest86cup -p 80:80 -v `pwd`:/usr/share/nginx/html:ro -d nginx:alpine
-
-echo "Done: Server listening on http://localhost:80/"
-
