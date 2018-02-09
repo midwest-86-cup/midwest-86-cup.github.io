445af4af65440f15cfce3a897956405114e68d8d
AJ Schalund <aschalund@thepei.com>
2018-02-09 00:19:20 -0600

    Fully functional single page website. Major update from existing master branch.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Midwest 86 Cup Website V2
+
+Updated version of the Midwest 86 Cup website built with Bulma CSS framework. Single page responsive website.
---
+++ js/main.js
@@ -0,0 +1,38 @@
+// The following code is based off a toggle menu by @Bradcomp
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+
+$(document).ready(function(){
+
+  $('.card').matchHeight();
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
